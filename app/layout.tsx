2f3460911eb6bcb417ec4d3d8ef0ee75512efa3a
Bmb-ai <alexboulboul@gmail.com>
2026-06-18 20:03:45 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./styles.css";
 
 export const metadata: Metadata = {
-  title: "Grand jeu pronostics football 2026 - Distri Concept",
-  description: "Participez au jeu concours de pronostics football 2026 et tentez de remporter un jeu de boules Obut."
+  title: "Grand jeu Coupe du monde 2026 - Distri Concept",
+  description: "Participez au jeu concours de pronostics et tentez de remporter un jeu de boules Obut."
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
